30f65fb830d9f105491951d56072cd23ae5ecb98
Lukas Mueller <lukemueller@protonmail.com>
2018-02-15 15:44:44 +0100

    Format code

--- src/Main.hs
+++ src/Main.hs
@@ -1,47 +1,47 @@
 module Main where
 
-import Chrono.TimeStamp
+import Data.Time.Clock.POSIX
 
---type Height = Int
---type Nonce = Int
---type PrevHash = String
---
---data Block = Block Height PrevHash
+-- QUESTION: What is strict v.s. lazy??
 
 data Block = Block
-  { height :: Int
-  , timestamp :: IO TimeStamp
-  , nonce :: Int
-  , prevHash :: String
-  }
+    { height    :: Int
+    , timestamp :: IO POSIXTime
+    , nonce     :: Int
+    , prevHash  :: String
+    }
 
 -- Creates the 'Genesis' block
 initBlock :: Block
 initBlock = Block
-  { height = 0
-  , timestamp = getCurrentTimeNanoseconds
-  , nonce = 0 -- Needs to be calculated
-  , prevHash = "0"
-  }
+    { height    = 0
+    , timestamp = getPOSIXTime
+    , nonce     = 0               -- Needs to be calculated
+    , prevHash  = "0"
+    }
 
 newBlock :: Block -> Block
 newBlock (Block{height = h}) = Block
-  { height = h + 1
-  , timestamp = getCurrentTimeNanoseconds
-  , nonce = 0       -- Needs to be calculated
-  , prevHash = "1"  -- Needs to be calculated
-  }
+    { height    = h + 1
+    , timestamp = getPOSIXTime
+    , nonce     =  3              -- Needs to be calculated
+    , prevHash  = "1"             -- Needs to be calculated
+    }
 
-data Blockchain = Blockchain { blocks :: [Block] }
+data Blockchain = Blockchain
+    { blocks :: [Block]
+    }
 
 initBlockchain :: Blockchain
-initBlockchain = Blockchain { blocks = [initBlock] }
+initBlockchain = Blockchain
+    { blocks = [initBlock]
+    }
 
 addBlock :: Blockchain -> Blockchain
-addBlock (Blockchain {blocks = b}) = Blockchain { blocks = (b ++ [last b]) }
+addBlock (Blockchain {blocks = b}) = Blockchain
+  { blocks = (b ++ [last b])
+  }
 
 main :: IO ()
 main = do
-  let d = addBlock $ initBlockchain
-
-  print "Hello"
+  print . floor =<< getPOSIXTime
