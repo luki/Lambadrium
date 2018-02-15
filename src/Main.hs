9c7a556c18ed4014651c2e13f4f6b1c25fa82c60
Lukas Mueller <lukemueller@protonmail.com>
2018-02-15 12:41:14 +0100

    Add basic methods

--- src/Main.hs
+++ src/Main.hs
@@ -1,4 +1,47 @@
 module Main where
 
+import Chrono.TimeStamp
+
+--type Height = Int
+--type Nonce = Int
+--type PrevHash = String
+--
+--data Block = Block Height PrevHash
+
+data Block = Block
+  { height :: Int
+  , timestamp :: IO TimeStamp
+  , nonce :: Int
+  , prevHash :: String
+  }
+
+-- Creates the 'Genesis' block
+initBlock :: Block
+initBlock = Block
+  { height = 0
+  , timestamp = getCurrentTimeNanoseconds
+  , nonce = 0 -- Needs to be calculated
+  , prevHash = "0"
+  }
+
+newBlock :: Block -> Block
+newBlock (Block{height = h}) = Block
+  { height = h + 1
+  , timestamp = getCurrentTimeNanoseconds
+  , nonce = 0       -- Needs to be calculated
+  , prevHash = "1"  -- Needs to be calculated
+  }
+
+data Blockchain = Blockchain { blocks :: [Block] }
+
+initBlockchain :: Blockchain
+initBlockchain = Blockchain { blocks = [initBlock] }
+
+addBlock :: Blockchain -> Blockchain
+addBlock (Blockchain {blocks = b}) = Blockchain { blocks = (b ++ [last b]) }
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = do
+  let d = addBlock $ initBlockchain
+
+  print "Hello"
