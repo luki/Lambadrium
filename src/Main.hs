599d8654237bc1b22734d332aa4be349fdb62044
Lukas Mueller <lukemueller@protonmail.com>
2018-02-16 15:40:55 +0100

    Finish hashBlock

--- src/Main.hs
+++ src/Main.hs
@@ -3,6 +3,11 @@ module Main where
 import Data.Time.Clock.POSIX
 import Data.Time.Format
 
+import Data.Char                (ord)
+import Data.ByteString          (unpack, pack)
+import Crypto.Hash.SHA256       as SHA256
+import Text.Printf              (printf)
+
 -- QUESTION: What is strict v.s. lazy??
 
 data Block = Block
@@ -34,12 +39,18 @@ newBlock (Block{height = h}) = Block
 -- 1. Hash them to a SHA256 string
 -- 2. Calculate a nonce
 hashBlock :: Block -> IO String
-hashBlock (Block{height = h, timestamp = t, nonce = n, prevHash = p})
-    = do
-      time <- t
-      let ts = formatTime defaultTimeLocale "%s" (posixSecondsToUTCTime time)
-      return $ show h ++ show n ++ p ++ ts
+hashBlock (Block{height = h, timestamp = t, nonce = n, prevHash = p}) = do
+    time <- t
+    let ts = formatTime defaultTimeLocale "%s" (posixSecondsToUTCTime time)
 
+    return $ hashStr $ show h ++ show n ++ p ++ ts
+
+hashStr :: String -> String
+hashStr s = concatMap (printf "%02x") $ -- To Hex Str
+            unpack $
+            hash $
+            pack $
+            map (fromIntegral.ord) s
 
 data Blockchain = Blockchain { blocks :: [Block] }
 
@@ -52,4 +63,9 @@ addBlock :: Blockchain -> Blockchain
 addBlock (Blockchain {blocks = b}) = Blockchain { blocks = (b ++ [last b]) }
 
 main :: IO ()
-main = print "It runs."
+main = do
+  let b = initBlock
+  str <- hashBlock b
+  print str
+
+  print "It runs."
