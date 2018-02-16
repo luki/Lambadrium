7179ddf9345e34cf40a040b12d84cb2cedd7265a
Lukas Mueller <lukemueller@protonmail.com>
2018-02-16 20:06:24 +0100

    Change prevHash of Block to IO String

--- src/Main.hs
+++ src/Main.hs
@@ -10,11 +10,14 @@ import Text.Printf              (printf)
 
 -- QUESTION: What is strict v.s. lazy??
 
+strToIOStr :: String -> IO String
+strToIOStr str = do return str
+
 data Block = Block
     { height    :: Int
     , timestamp :: IO POSIXTime
     , nonce     :: Int
-    , prevHash  :: String
+    , prevHash  :: IO String
     }
 
 -- Creates the 'Genesis' block
@@ -23,27 +26,28 @@ initBlock = Block
     { height    = 0
     , timestamp = getPOSIXTime
     , nonce     = 0               -- Needs to be calculated
-    , prevHash  = "0"
+    , prevHash  = strToIOStr "0"
     }
 
-
+-- Creates a new block with a prev. block
 newBlock :: Block -> Block
-newBlock (Block{height = h}) = Block
+newBlock (b@Block{height = h}) = Block
     { height    = h + 1
     , timestamp = getPOSIXTime
     , nonce     =  3              -- Needs to be calculated
-    , prevHash  = "1"             -- Needs to be calculated
+    , prevHash  = hashBlock b
     }
 
 -- | TODO
--- 1. Hash them to a SHA256 string
--- 2. Calculate a nonce
+-- 1. Calculate a nonce
 hashBlock :: Block -> IO String
 hashBlock (Block{height = h, timestamp = t, nonce = n, prevHash = p}) = do
+    prevHash <- p
     time <- t
     let ts = formatTime defaultTimeLocale "%s" (posixSecondsToUTCTime time)
-    let hash = hashStr $ show h ++ show n ++ p ++ ts
+    return $ hashStr $ show h ++ show n ++ prevHash ++ ts
 
+-- A function to hash a String (with SHA256)
 hashStr :: String -> String
 hashStr s = concatMap (printf "%02x") $ -- To Hex Str
             unpack $
@@ -63,8 +67,8 @@ addBlock (Blockchain {blocks = b}) = Blockchain { blocks = (b ++ [last b]) }
 
 main :: IO ()
 main = do
-  let b = initBlock
-  str <- hashBlock b
-  print str
+  -- let b = initBlock
+  -- str <- hashBlock b
+  -- print str
 
   print "It runs."
