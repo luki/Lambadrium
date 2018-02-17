a40053d63251d41fbcbc11c69a2ec1802cb68a0d
Lukas Mueller <lukemueller@protonmail.com>
2018-02-17 16:47:36 +0100

    Implement mining

--- src/Main.hs
+++ src/Main.hs
@@ -8,10 +8,17 @@ import Data.ByteString          (unpack, pack)
 import Crypto.Hash.SHA256
 import Text.Printf              (printf)
 
--- QUESTION: What is strict v.s. lazy??
+-- strToIOStr :: String -> IO String
+-- strToIOStr str = do return str
 
-strToIOStr :: String -> IO String
-strToIOStr str = do return str
+takeNElements :: (Show a) => [a] -> Int -> [a]
+takeNElements [] _      = []
+takeNElements _ 0       = []
+takeNElements (x:xs) n  = x : (takeNElements xs $ n-1)
+
+repeatElem :: (Show a) => a -> Int -> [a]
+repeatElem _ 0  = []
+repeatElem a n  = a : (repeatElem a $ n-1)
 
 data Block = Block
     { height    :: Int
@@ -45,7 +52,7 @@ newBlock (b@Block{height = h}) time = Block
 hashBlock :: Block -> String
 hashBlock (Block{height = h, timestamp = t, nonce = n, prevHash = p}) =
     let ts = formatTime defaultTimeLocale "%s" (posixSecondsToUTCTime t)
-     in hashStr $ show h ++ show n ++ p ++ ts
+      in hashStr $ show h ++ show n ++ p ++ ts
 
 -- A function to hash a String (with SHA256)
 hashStr :: String -> String
@@ -56,15 +63,34 @@ hashStr s =
     pack $
     map (fromIntegral.ord) s
 
-data Blockchain = Blockchain { blocks :: [Block] }
+calculateNonce :: Block -> Int -> Block
+calculateNonce (b@Block{height = h, timestamp = t, nonce = n, prevHash = p}) d
+    | firstOfHash == difRequirement = b
+    | otherwise = calculateNonce newBlock d
+  where
+      difRequirement = repeatElem '0' d
+      firstOfHash    = hashBlock b `takeNElements` d
+      newBlock       = Block { height = h, timestamp = t, nonce = n + 1, prevHash = p }
+
+data Blockchain = Blockchain
+    { blocks     :: [Block]
+    , difficulty :: Int
+    }
 
 initBlockchain :: IO Blockchain
 initBlockchain = do
     i <- initBlock
-    return $ Blockchain { blocks = [i] }
+    return $ Blockchain
+        { blocks = [i]
+        , difficulty = 0
+        }
 
 addBlock :: Blockchain -> IO Blockchain
-addBlock (Blockchain {blocks = b}) = new >>= \n -> return $ Blockchain { blocks = n : b }
+addBlock (Blockchain {blocks = b, difficulty = d}) =
+    new >>= \n -> return $ Blockchain
+        { blocks = n : b
+        , difficulty = d
+        }
    where
        new = do
            let l = last b
@@ -73,7 +99,16 @@ addBlock (Blockchain {blocks = b}) = new >>= \n -> return $ Blockchain { blocks 
 
 main :: IO ()
 main = do
-    chain <- initBlockchain
-    newChain <- addBlock chain
-    putStrLn . hashBlock . head . blocks $ newChain
+    time <- getPOSIXTime
+    genesis <- initBlock
+    let b = newBlock genesis time
+    let final_b = calculateNonce b 4
+    
+    putStrLn $ hashBlock b
+    putStrLn $ hashBlock final_b
+
+
+    -- chain <- initBlockchain
+    -- newChain <- addBlock chain
+    -- putStrLn . hashBlock . head . blocks $ newChain
     putStrLn "Runs :D"
